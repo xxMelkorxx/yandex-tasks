0902d631f0f28bc8859e2be9c90c8634b9842c30
Melkor <xxxserborxxx@gmail.com>
2023-12-14 20:17:10 +0300

    add functions for the second task

--- task2.py
+++ task2.py
@@ -16,8 +16,63 @@
 Формат вывода:
     Выведите n чисел: искомая перестановка индексов от 1 до n. 
     Если существует несколько решений, выведите любое из них. Числа в строке следует разделять пробелами.
+
+Примечания:
+Инверсией в перестановке p порядка n называется всякая пара индексов (i, j) такая, что 1 ≤ i < j ≤ n и 
+pi > pj. Чётность числа инверсий в перестановке определяет чётность перестановки.
 """
 
+from enum import Enum
+
+class Plane(Enum):
+    Oxy = 'Oxy'
+    Oyz = 'Oyz'
+    Oxz = 'Ozx'
+
+class Points:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f"{self.x} {self.y} {self.z}"
+
+# Рассчёт дистанции между точками на одно из плоскостей.
+def distance(p1: Points, p2: Points, plane: Plane) -> float:
+    if plane == Plane.Oxy:
+        return ((p2.y - p1.y) ** 2 + (p2.x - p1.x) ** 2) ** 0.5
+    if plane == Plane.Oyz:
+        return ((p2.y - p1.y) ** 2 + (p2.z - p1.z) ** 2) ** 0.5
+    if plane == Plane.Oxz:
+        return ((p2.x - p1.x) ** 2 + (p2.z - p1.z) ** 2) ** 0.5
+
+# Проверка, является ли последовательность точек на плоскости тривиальной.
+def is_trivial(points: list, plane: Plane) -> bool:
+    curr_d = 0
+    for i in range(1, len(points)):
+        d = distance(points[0], points[i], plane)
+        print(f"p0: {points[0]}, pi: {points[i]}, d = {d}, plane - {plane.value}")
+        if d > curr_d:
+            curr_d = d
+        else:
+            return False
+    return True
+
+# Проверка, является ли последовательность точек хорошей.
+def is_good(points: list) -> bool:
+    is_triv_xy = is_trivial(points, Plane.Oxy)
+    print(f"Последовательность тривиальная? {is_triv_xy}")
+    is_triv_yz = is_trivial(points, Plane.Oyz)
+    print(is_triv_yz)
+    is_triv_xz = is_trivial(points, Plane.Oxz)
+    print(is_triv_xz)
+    return not (is_triv_xy and is_triv_yz and is_triv_xz)
+
 if __name__ == '__main__':
-    n = int(input())
-    points = [[int(i) for i in input().split()] for _ in range(n)]
+    n, list_p = int(input()), []
+    # Получение точек.
+    for _ in range(n):
+        p = input().split()
+        list_p += [Points(int(p[0]), int(p[1]), int(p[2]))]
+    print(f"Является ли последовательность хорошей? - {is_good(list_p)}")
